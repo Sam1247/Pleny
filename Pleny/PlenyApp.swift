ff9e3bc186ebf440035dcace7f3a9f4d16dd129e
Abdalla Elsaman <abdallaelsaman67@gmail.com>
2025-03-14 15:01:04 +0200

    Initial Commit

---
+++ Pleny/PlenyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyApp.swift
+//  Pleny
+//
+//  Created by Abdalla Elsaman on 14/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlenyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
